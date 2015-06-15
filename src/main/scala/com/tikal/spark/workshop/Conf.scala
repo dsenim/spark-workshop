b44b606bf4f3aa684fefee2eb76a5221bf749dbc
dsenim <dmitri.krasnenko@gmail.com>
2015-06-15 23:51:30 +0300

    Added sample data.

--- src/main/scala/com/tikal/spark/workshop/Conf.scala
+++ src/main/scala/com/tikal/spark/workshop/Conf.scala
@@ -9,6 +9,13 @@ import org.slf4j.{Logger, LoggerFactory}
  * @author Dmitri Krasnenko
  */
 object Conf {
+  val SPARK_DATA = "/var/lib/spark/data"
+
+  val NAMES_PATH = s"$SPARK_DATA/names"
+  val HELLO_PATH = s"$SPARK_DATA/hello.txt"
+  val NAMES_RAWS_PATH = s"$SPARK_DATA/names.txt"
+  val NAMES_JSON_PATH = s"$SPARK_DATA/names.json"
+
   val MAPPER: ObjectMapper = new ObjectMapper()
 
   val LOGGER:Logger = LoggerFactory.getLogger("com.tikal.spark.workshop")
